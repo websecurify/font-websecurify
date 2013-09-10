2abe0fdfaab8030a3f0e550c1b6130a2f0d21718
Petko D. Petkov <pdp@hamstgsql01.uk.betfair.local>
2013-09-10 16:21:58 +0100

    Upgraded to new font.

--- lte-ie7.js
+++ lte-ie7.js
@@ -3,25 +3,29 @@
 window.onload = function() {
 	function addIcon(el, entity) {
 		var html = el.innerHTML;
-		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+		el.innerHTML = '<span style="font-family: \'Font-Websecurify\'">' + entity + '</span>' + html;
 	}
 	var icons = {
-			'icon-faviconxmlfuzz' : '&#xe001;',
-			'icon-faviconwpscan' : '&#xe004;',
-			'icon-faviconwebsecurify' : '&#xe005;',
-			'icon-faviconscanner' : '&#xe007;',
-			'icon-faviconbackframe' : '&#xe002;',
-			'icon-faviconclassic' : '&#xe009;',
-			'icon-faviconrforge' : '&#xe00a;',
-			'icon-faviconretest' : '&#xe00b;',
-			'icon-faviconrest' : '&#xe00c;',
-			'icon-faviconresend' : '&#xe00d;',
-			'icon-faviconrecon' : '&#xe00f;',
-			'icon-faviconreburp' : '&#xe010;',
-			'icon-faviconpublic' : '&#xe011;',
-			'icon-faviconprojects' : '&#xe012;',
-			'icon-faviconfoundation' : '&#xe014;',
-			'icon-faviconhttpview' : '&#xe015;'
+			'websecurify-icon-faviconxmlfuzz' : '&#xe012;',
+			'websecurify-icon-faviconwpscan' : '&#xe013;',
+			'websecurify-icon-faviconwebsecurify' : '&#xe005;',
+			'websecurify-icon-faviconscanner' : '&#xe006;',
+			'websecurify-icon-faviconrforge' : '&#xe007;',
+			'websecurify-icon-faviconretest' : '&#xe008;',
+			'websecurify-icon-faviconrest' : '&#xe009;',
+			'websecurify-icon-faviconresend' : '&#xe000;',
+			'websecurify-icon-faviconrecon' : '&#xe001;',
+			'websecurify-icon-faviconreburp' : '&#xe00f;',
+			'websecurify-icon-faviconpublic' : '&#xe00a;',
+			'websecurify-icon-faviconprojects' : '&#xe00b;',
+			'websecurify-icon-faviconlaunch' : '&#xe00c;',
+			'websecurify-icon-faviconjsonfuzz' : '&#xe00d;',
+			'websecurify-icon-faviconhttpview' : '&#xe00e;',
+			'websecurify-icon-faviconhaxor' : '&#xe010;',
+			'websecurify-icon-faviconfoundation' : '&#xe011;',
+			'websecurify-icon-faviconclassic' : '&#xe002;',
+			'websecurify-icon-faviconbackframe' : '&#xe003;',
+			'websecurify-icon-faviconarena' : '&#xe004;'
 		},
 		els = document.getElementsByTagName('*'),
 		i, attr, c, el;
@@ -35,7 +39,7 @@ window.onload = function() {
 			addIcon(el, attr);
 		}
 		c = el.className;
-		c = c.match(/icon-[^\s'"]+/);
+		c = c.match(/websecurify-icon-[^\s'"]+/);
 		if (c && icons[c[0]]) {
 			addIcon(el, icons[c[0]]);
 		}
